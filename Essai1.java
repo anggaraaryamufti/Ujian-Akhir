a3b319f7c34f968ff62dfd3c226d516c84ce0036
anggaraaryamufti <87553627+anggaraaryamufti@users.noreply.github.com>
2021-07-18 02:52:30 +0700

    Update Essai1.java

--- Essai1.java
+++ Essai1.java
@@ -8,24 +8,24 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class Essai1 extends AppCompatActivity {
-    TextView sebuahTeks;
+    TextView teks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sebuahTeks = findViewById(R.id.teks);
+        teks = findViewById(R.id.teks);
 
-        final Button tombolApaDeh = findViewById(R.id.tombol);
-        tombolApaDeh.setOnClickListener(new View.OnClickListener() {
+        final Button tombol = findViewById(R.id.tombol);
+        tombol.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // terserah mau apa gitu
-                tombolDipencet(v);
+                
+               clickedbutton(v);
             }
         });
 
         setContentView(R.layout.activity_essai2);
 
-    }void tombolDipencet(View v){
-        sebuahTeks.setText("Tombol sudah dipencet");
+    }void clickedbutton(View v){
+        teks.setText("Tombol sudah dipencet");
     }
 }
